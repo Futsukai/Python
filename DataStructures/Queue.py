a797bd84b78814b744109a041dae3df4ac49b227
sixGod <z.o.e@outlook.com>
2020-05-28 15:29:18 +0800

    commit: 修正代码错误

--- DataStructures/Queue.py
+++ DataStructures/Queue.py
@@ -30,7 +30,7 @@ class Deque(object):
         self.items.append(item)
 
     def add_rear(self, item):
-        self.items.insert(o, item)
+        self.items.insert(0, item)
 
     def remove_front(self):
         return self.items.pop()
