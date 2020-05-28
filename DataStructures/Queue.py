6f5ce24b8e540ab575df26cc8f5cbe26dddf5f4b
sixGod <z.o.e@outlook.com>
2020-05-28 15:20:30 +0800

    commit: 新增队列，双端队列

---
+++ DataStructures/Queue.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+#!/usr/bin/python3
+
+
+class Queue(object):
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+
+class Deque(object):
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def add_front(self, item):
+        self.items.append(item)
+
+    def add_rear(self, item):
+        self.items.insert(o, item)
+
+    def remove_front(self):
+        return self.items.pop()
+
+    def remove_rear(self):
+        return self.item.pop(0)
+
+    def size(self):
+        return len(self.items)
